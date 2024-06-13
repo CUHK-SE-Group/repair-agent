b63b119a16be338b5a09fa59c98f687f4379c85a
Siyuexi <siyuexi@foxmail.com>
2024-06-13 17:40:26 +0800

    --Update=README

--- batch.sh
+++ batch.sh
@@ -1,3 +1,15 @@
+# ///////////////////////////////////////////////////////////////////////////////#
+# NOTE: The following setting is for remote model (GPT-4)                        #
+# If you want to use local model (Mixtral),                                      #
+# you should delete `--api_key` argument,                                        #
+# change `--remote_model` argument to `--local_model`,                           #
+# change `model='gpt-4'` to `model='mixtral-8x7b-instruct'`,                     #
+# and add `--chat_mode local` argument to the following commands.                #
+# Before using Mixtral,                                                          #
+# you should download the model from Hugging Face and place it in this directory.#
+# ///////////////////////////////////////////////////////////////////////////////#
+
+# Settings
 api_key='sk-xxx'
 model='gpt-4'
 max_try=10
